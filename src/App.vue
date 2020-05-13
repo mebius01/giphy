aad429bbbb18d12ed30268bc22d356317284940c
iv <consmebius@gmail.com>
2020-05-13 22:57:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-icon large>account_circle</v-icon>
+      <v-spacer></v-spacer>
+      <Search />
+    </v-app-bar>
+    <main class="grid"></main>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Saerch";
+export default {
+  name: "App",
+  components: {
+    Search
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/Saerch.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col @keydown>
+      <v-text-field label="Search" :value="tic" @input="search = $event.terget.value"></v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      search: ""
+    };
+  }
+};
+</script>
